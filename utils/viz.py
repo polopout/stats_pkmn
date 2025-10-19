cee03c60f7b633a2de44e1a96f06097d1fd1af56
polopout <rpaulantoine@gmail.com>
2025-10-19 13:28:05 +0200

    ff

--- utils/viz.py
+++ utils/viz.py
@@ -350,7 +350,9 @@ from pathlib import Path
 import streamlit as st
 
 # chemin relatif depuis le fichier courant
-IMAGE_PATH = Path(__file__).parent.parent / "data" / "pub2.png"
+RACINE_FOLDER=Path(__file__).parent.parent 
+DATA_FOLDER=os.path.join(RACINE_FOLDER,"data")
+IMAGE_PATH = os.path.join(RACINE_FOLDER, "pub2.png")
 
 def sidebar_logo():
     st.sidebar.image(str(IMAGE_PATH), use_container_width=True)
